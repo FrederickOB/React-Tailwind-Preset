796341ddc477e32abe921a05f82c59a146595d7f
Frederick Ofori-Boadu <friedosei@gmail.com>
2020-08-07 12:35:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./tailwind.output.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-300">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
